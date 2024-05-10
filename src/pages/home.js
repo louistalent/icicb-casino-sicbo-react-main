951f431d797463d3b2a0d312fb55d62e284404de
goodman <topdeveloper01234@gmail.com>
2024-05-10 20:08:33 +0800

    first commit

---
+++ src/pages/home.js
@@ -0,0 +1,47 @@
+import React, { useEffect } from "react";
+import Unity, { UnityContext } from "react-unity-webgl";
+import jwtDecode from "jwt-decode";
+
+const unityContext = new UnityContext({
+    loaderUrl: "build/Build/build.loader.js",
+    dataUrl: "build/Build/build.data",
+    frameworkUrl: "build/Build/build.framework.js",
+    codeUrl: "build/Build/build.wasm"
+});
+
+function Home() {
+    useEffect(() => {
+        unityContext.on("GameReady", function (message) {
+            if (message === "Ready") {
+                try {
+                    window.onmessage = (e) => {
+                        if (e.data.name === "iframe_message") {
+                            let user = jwtDecode(e.data.token);
+                            unityContext.send("GameManager", "RequestToken", JSON.stringify({
+                                username: user,
+                                token: e.data.token,
+                                amount: e.data.allowanceAmount
+                            }));
+                        }
+                    }
+                    window.parent.postMessage({ name: "iframe_message" }, "*");
+                } catch (err) {
+                    console.log("error", err);
+                }
+            }
+            else console.log("error");
+        });
+    }, []);
+
+    return (
+        <div>
+            <Unity
+                unityContext={unityContext}
+                matchWebGLToCanvasSize={true}
+                style={{ height: "90vh", marginTop: "5vh", width: "100%" }}
+            />
+        </div>
+    );
+}
+
+export default Home
